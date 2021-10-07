9bb6ce4f392ecc98f9cb336b5fd5c9f065133d17
Michael Pejskar <xpejsk00@vutbr.cz>
2021-10-07 14:39:16 +0200

    Hanoj - dodelam

---
+++ hanoj.R
@@ -0,0 +1,8 @@
+#Michael Pejskar PRG cv 3
+rm(list = ls())
+HanojVez <- function(n){
+  kolik_1 <- matrix(0,nrow=1,ncol=n)
+  kolik_2 <- matrix(0,nrow=1,ncol=n)
+  kolik_3 <- matrix(0,nrow=1,ncol=n)
+  kolik_1[]
+}
